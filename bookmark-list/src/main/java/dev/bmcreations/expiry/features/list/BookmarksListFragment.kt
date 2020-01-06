6044fbc09054d9ebd3c5c5d9fe3e680d2b6bdf59
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-01-06 05:42:07 -0500

    feat: create bookmark-list feature module stub
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

---
+++ bookmark-list/src/main/java/dev/bmcreations/expiry/features/list/BookmarksListFragment.kt
@@ -0,0 +1,12 @@
+package dev.bmcreations.expiry.features.list
+
+import dev.bmcreations.expiry.base.ui.BaseFragment
+
+class BookmarksListFragment: BaseFragment() {
+
+    override val layoutResId: Int = R.layout.fragment_bookmark_list
+
+    override fun initView() {
+
+    }
+}
