b4b0f8b6431d8c01c0b5c16b504973e960e437f8
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-01-06 06:24:27 -0500

    style: theme app
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- bookmark-list/src/main/java/dev/bmcreations/expiry/features/list/BookmarksListFragment.kt
+++ bookmark-list/src/main/java/dev/bmcreations/expiry/features/list/BookmarksListFragment.kt
@@ -1,12 +1,36 @@
 package dev.bmcreations.expiry.features.list
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import dev.bmcreations.expiry.base.ui.BaseFragment
 
 class BookmarksListFragment: BaseFragment() {
 
     override val layoutResId: Int = R.layout.fragment_bookmark_list
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun initView() {
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_app_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.search -> {
+                // TODO: search
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
