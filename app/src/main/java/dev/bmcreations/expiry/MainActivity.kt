a6f3c0852b7e36d5c03a03f0a681267a83ee58f8
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-01-08 10:36:24 -0500

    style: set actionbar to color_surface
    allow primary color to be different
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- app/src/main/java/dev/bmcreations/expiry/MainActivity.kt
+++ app/src/main/java/dev/bmcreations/expiry/MainActivity.kt
@@ -1,13 +1,16 @@
 package dev.bmcreations.expiry
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.bmcreations.expiry.core.extensions.colors
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(colors[R.color.color_background]))
     }
 }
