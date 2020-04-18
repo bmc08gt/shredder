7cddfcc590acc7394c28779a00f01eefc551692e
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-04-18 18:13:37 -0400

    feat(userprefs): maintain logged in state via prefs
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- core/src/main/java/dev/bmcreations/shredder/core/preferences/UserPreferences.kt
+++ core/src/main/java/dev/bmcreations/shredder/core/preferences/UserPreferences.kt
@@ -1,6 +1,32 @@
 package dev.bmcreations.shredder.core.preferences
 
 import android.content.Context
+import androidx.core.content.edit
+import com.google.gson.Gson
+import dev.bmcreations.shredder.models.UserLogin
 
 class UserPreferences(appContext: Context) {
+
+    private val gson = Gson()
+
+    var user: UserLogin? = null
+        set(value) {
+            field = value
+            prefs.edit { putString(USER, gson.toJson(value, UserLogin::class.java)) }
+            isLoggedIn = value != null
+        }
+
+    var isLoggedIn: Boolean = false
+        private set
+
+    private val prefs = appContext.getSharedPreferences("shredder", Context.MODE_PRIVATE)
+
+    init {
+        val userString = prefs.getString(USER, null)
+        user = gson.fromJson(userString, UserLogin::class.java)
+    }
+
+    companion object {
+        private const val USER = "user_login"
+    }
 }
